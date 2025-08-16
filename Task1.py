2578a6203925701227ee90d21c3091ab6081b68d
snehakalawat <211883714+snehakalawat@users.noreply.github.com>
2025-08-16 21:46:53 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,15 @@
+First = int(input("Enter a First Number: "));
+Second = int(input("Enter the Second Number: "));
+
+add = First + Second ;
+sub = First - Second;
+multiply = First * Second;
+divide = First / Second;
+
+print("Addition: ", add);
+print("Substraction: ", sub);
+print("Division: ", divide);
+print("Multiplication: ",multiply);
+
+
+
