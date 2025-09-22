3318363f13fdf81c386061d74aab25613e61d7ca
snehakalawat <211883714+snehakalawat@users.noreply.github.com>
2025-09-22 19:36:44 +0530

    Add files via upload

---
+++ Task6.py
@@ -0,0 +1,14 @@
+import math
+
+
+num = float(input("Enter a number: "))
+
+
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+
+print(f"Square root of {num} = {square_root}")
+print(f"Natural logarithm of {num} = {natural_log}")
+print(f"Sine of {num} radians = {sine_value}")
