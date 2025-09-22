0f8b3fd7bb9d5479fdbbf4ce6ccca5c4f84ef5e5
snehakalawat <211883714+snehakalawat@users.noreply.github.com>
2025-09-22 19:44:00 +0530

    Add files via upload

---
+++ Task10.py
@@ -0,0 +1,12 @@
+
+numbers = list(range(1, 11))
+
+
+first_five = numbers[:5]
+
+
+reversed_list = first_five[::-1]
+
+
+print("First five elements:", first_five)
+print("Reversed list:", reversed_list)
